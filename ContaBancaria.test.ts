81e30c9d0fc55d7fd34dbefc29820447d116d004
Ana Lívia Farias S <131826367+anafariass@users.noreply.github.com>
2024-09-13 15:44:26 -0300

    Add files via upload
    Fazendo testes de Conta Bancária

--- ContaBancaria.test.ts
+++ ContaBancaria.test.ts
@@ -1,4 +1,33 @@
-// import { beforeEach, describe, expect, test } from "bun:test";
-// import ContaBancaria from "./ContaBancaria.ts";
+import { beforeEach, describe, expect, test } from "bun:test";
+import ContaBancaria from "./ContaBancaria.ts";
 
 
+describe("Testando classe ContaBancaria", () => {
+  let conta: ContaBancaria;
+  beforeEach(() => {
+    conta = new ContaBancaria();
+  });
+
+  test("Testando depositar valor positivo", () => {
+    conta.depositar(100);
+    expect(conta.consultarSaldo()).toBe(100);
+  });
+
+  test("Testando depositar valor negativo", () => {
+    conta.depositar(-100);
+    expect(conta.consultarSaldo()).toBe(0);
+  });
+
+  test("Testando saque válido", () => {
+    conta.depositar(100);
+    conta.sacar(50)
+    expect(conta.consultarSaldo()).toBe(50);
+  });
+
+  test("Lançando erro", () => {
+    conta.depositar(100);
+    expect (() => {
+      conta.sacar(150);
+    }).toThrow("Saldo insuficiente")
+  });
+});
--- ContaBancaria.ts
+++ ContaBancaria.ts
@@ -1,1 +1,41 @@
-export default class ContaBancaria {}
+export default class ContaBancaria {
+  private numeroConta : number;
+  private saldo = 0;
+  private agencia: 1 ;
+  private extrato: string[];
+
+  public constructor() {
+    this.numeroConta = 0
+    this.agencia = 1
+    this.extrato = []
+  }
+
+  public depositar(valor: number) {
+    if (valor > 0) {
+      this.saldo += valor;
+    }
+  }
+
+  public sacar(valor:number){
+    if (this.saldo >= valor){
+        this.saldo -= valor;
+        return valor;
+
+    }
+
+    //return throw new Error("Saldo insuficiente")
+
+  }
+  public numberConta(valor:number){
+    return this.numeroConta 
+  }
+
+  public consultarSaldo() {
+    return this.saldo;
+  }
+
+
+
+
+}
+
